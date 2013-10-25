0ce377467559358cff6e199d81a77d3c9b9b9eae
Carlton Whitehead <cebesius86@gmail.com>
2013-10-25 09:06:09 -0400

    First stab at \Peppercorn\St1\File

--- Peppercorn/St1/File.php
+++ Peppercorn/St1/File.php
@@ -3,5 +3,39 @@ namespace Peppercorn\St1;
 
 class File
 {
-    // TODO
+
+    /**
+     * Contains all lines from this file
+     * @var array
+     */
+    private $lines;
+
+    public function __construct($content)
+    {
+        $this->lines = self::splitContentIntoLines($content);
+    }
+
+    private static function splitContentIntoLines($content) {
+        $rawLines = explode("\n", $content);
+        $lines = array();
+        foreach ($rawLines as $rawLine) {
+            $line = trim($rawLine);
+            if (strlen($line) > 0) { // omit empty lines
+                $lines[] = $line;
+            }
+        }
+        return $lines;
+    }
+
+    public function getLineCount() {
+        return count($this->lines);
+    }
+
+    public function getLine($index) {
+        assert('is_integer($index)');
+        assert('$index >= 0');
+        assert('$index < $this->getLineCount()');
+        return $this->lines[$index];
+    }
+
 }
