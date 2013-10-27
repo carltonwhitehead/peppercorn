0080f676345b38364d625942ef2c53620b5e026a
Carlton Whitehead <cebesius86@gmail.com>
2013-10-27 19:11:10 -0400

    Construct lines in Peppercorn\St1\File ctor

--- Peppercorn/St1/File.php
+++ Peppercorn/St1/File.php
@@ -9,29 +9,46 @@ class File
 {
 
     /**
-     * Contains all lines from this file
-     *
+     * An array of Line objects, each representing a line from $content
      * @var array
      */
-    private $lines;
+    private $lines = array();
 
     public function __construct($content)
     {
-        Preconditions::checkArgument(Strings::isNotEmpty($content));
-        $this->lines = self::splitContentIntoLines($content);
+        $stringLines = self::splitContentIntoStringLines($content);
+        self::buildAndSetLines($stringLines);
     }
 
-    private static function splitContentIntoLines($content)
+    /**
+     * Split the content passed to the constructor into an array of processed string lines
+     *
+     * @param array $content
+     * @return array:
+     */
+    private static function splitContentIntoStringLines($content)
     {
-        $rawLines = explode("\n", $content);
-        $lines = array();
+        $rawLines = explode(PHP_EOL, $content);
+        $stringLines = array();
         foreach ($rawLines as $rawLine) {
-            $line = trim($rawLine);
+            $stringLine = trim($rawLine);
             if (Strings::isNotEmpty($line)) {
-                $lines[] = $line;
+                $stringlines[] = $stringLine;
             }
         }
-        return $lines;
+        return $stringLines;
+    }
+
+    /**
+     * Build and set Line objects for each value in $stringLines
+     * @param array $stringLines
+     * @return array
+     */
+    private static function buildAndSetLines($stringLines)
+    {
+        foreach ($stringsLines as $stringLine) {
+            $this->lines[] = new Line($this, $stringLine);
+        }
     }
 
     public function getLineCount()
