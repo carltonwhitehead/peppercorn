ebcb918e689c901ed713ab713299f492bacea33f
Carlton Whitehead <cebesius86@gmail.com>
2013-11-01 21:11:54 -0400

    Add documentation and preconditions to Category

--- Peppercorn/St1/Category.php
+++ Peppercorn/St1/Category.php
@@ -1,16 +1,29 @@
 <?php
 namespace Peppercorn\St1;
 
+use Phava\Base\Preconditions;
 class Category
 {
 
+    /**
+     * the prefix of the category as stored in the .st1 file
+     * @var string
+     */
     private $prefix;
 
+    /**
+     * @var string
+     */
     public function __construct($prefix)
     {
+        Preconditions::checkArgumentIsString($prefix);
         $this->prefix = $prefix;
     }
 
+    /**
+     * get the prefix of the category as stored in the .st1 file
+     * @return string
+     */
     public function getPrefix()
     {
         return $this->prefix;
