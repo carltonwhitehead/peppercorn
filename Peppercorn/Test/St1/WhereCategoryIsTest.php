b74fea475b24ba0f2847c9b055d02cbac16df4b4
Carlton Whitehead <cebesius86@gmail.com>
2013-11-17 19:47:11 -0500

    Stub tests for WhereCategoryIs and WhereClassIs

---
+++ Peppercorn/Test/St1/WhereCategoryIsTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Peppercorn\Test\St1;
+
+use Peppercorn\St1\Category;
+use Peppercorn\St1\WhereCategoryIs;
+
+class WhereCategoryIsTest extends \PHPUnit_Framework_TestCase
+{
+    // TODO: proper test coverage
+
+    public function testTodo()
+    {
+        new WhereCategoryIs(new Category(''));
+    }
+}
---
+++ Peppercorn/Test/St1/WhereClassIsTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Peppercorn\Test\St1;
+
+use Peppercorn\St1\Category;
+use Peppercorn\St1\WhereClassIs;
+
+class WhereClassIsTest extends \PHPUnit_Framework_TestCase
+{
+    // TODO: proper test coverage
+
+    public function testTodo()
+    {
+        new WhereClassIs(new Category(''), 'STR');
+    }
+}
