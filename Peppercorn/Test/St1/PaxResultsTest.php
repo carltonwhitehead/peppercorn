be4f10046986e89d0b8952ad2b7055ce3f86ea81
Carlton Whitehead <cebesius86@gmail.com>
2013-12-08 21:30:34 -0500

    Add pax result tests for 2013 points 3

--- Peppercorn/Test/St1/PaxResultsTest.php
+++ Peppercorn/Test/St1/PaxResultsTest.php
@@ -80,6 +80,28 @@ class PaxResultsTest extends ResultsTest
         $provider[] = array($result, 92, 'SM', '10', '52.333');
         $provider[] = array($result, 93, 'SM', '9', '53.710');
         $provider[] = array($result, 94, 'XSTS', '15', 'DSQ');
+        // points3
+        $result = $results[2];
+        $provider[] = array($result, 0, 'RTFSRT', '6', '37.798');
+        $provider[] = array($result, 1, 'STR', '0', '38.246');
+        $provider[] = array($result, 2, 'DS', '78', '38.551');
+        $provider[] = array($result, 3, 'DS', '15', '38.568');
+        $provider[] = array($result, 4, 'STX', '44', '38.911');
+        $provider[] = array($result, 5, 'RTCSRT', '5', '38.934');
+        $provider[] = array($result, 6, 'RTHSRT', '7', '38.971');
+        $provider[] = array($result, 7, 'SS', '1', '39.317');
+        $provider[] = array($result, 8, 'STR', '80', '39.536');
+        $provider[] = array($result, 9, 'RTCSRT', '14', '39.543');
+        $provider[] = array($result, 19, 'DM', '14', '40.404');
+        $provider[] = array($result, 29, 'RTGSRT', '247', '41.602');
+        $provider[] = array($result, 39, 'SSM', '2', '42.426');
+        $provider[] = array($result, 49, 'NOVFS', '88', '43.796');
+        $provider[] = array($result, 59, 'RTFPRT', '16', '44.714');
+        $provider[] = array($result, 69, 'NOVDS', '221', '45.538');
+        $provider[] = array($result, 79, 'RTHSRT', '4', '46.570');
+        $provider[] = array($result, 86, 'NOVFP', '61', '48.861');
+        $provider[] = array($result, 87, 'STC', '87', '49.154');
+        $provider[] = array($result, 88, 'NOVDM', '44', '49.192');
         
         return $provider;
     }
