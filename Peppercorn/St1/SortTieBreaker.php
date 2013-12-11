abe1efd1575a61c34c7b72e9844b11efea3ac9f3
Carlton Whitehead <cebesius86@gmail.com>
2013-12-11 17:03:10 -0500

    Clean up an ugly while loop

--- Peppercorn/St1/SortTieBreaker.php
+++ Peppercorn/St1/SortTieBreaker.php
@@ -11,27 +11,21 @@ abstract class SortTieBreaker
     public final function findAndBreakTies(callable $sort, array &$lines)
     {
         $tiesBroken = [];
-        $stopI = count($lines) - 1;
-        $brokeTies = false;
         $startI = 0;
-        while ($brokeTies === false) {
-            $brokeTies = false;
-            for ($i = $startI; $i < $stopI; $i++) {
-                $a = $lines[$i];
-                $b = $lines[$i + 1];
-                if ($sort($a, $b) === 0) {
-                    $tieBreak = $this->breakTie($a, $b);
-                    if ($tieBreak->wentToB()) {
-                        $lines[$i] = $b;
-                        $lines[$i + 1] = $a;
-                        $brokeTies = true;
-                        $startI = $i - 1;
-                        break;
-                    }
-                    $tiesBroken[] = $tieBreak;
+        $stopI = count($lines) - 1;
+        for ($i = $startI; $i < $stopI; $i++) {
+            $a = $lines[$i];
+            $b = $lines[$i + 1];
+            if ($sort($a, $b) === 0) {
+                $tieBreak = $this->breakTie($a, $b);
+                if ($tieBreak->wentToB()) {
+                    $lines[$i] = $b;
+                    $lines[$i + 1] = $a;
+                    $i = max(0, $i - 2);
+                    continue;
                 }
+                $tiesBroken[] = $tieBreak;
             }
-            $brokeTies = true;
         }
         return $tiesBroken;
     }
