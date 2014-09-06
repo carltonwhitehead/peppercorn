d1bffc689013f6d4726c42557b2e0868bb5bca34
Carlton Whitehead <cebesius86@gmail.com>
2014-09-05 21:11:43 -0400

    Cache runs within PAX tie breaker

--- Peppercorn/St1/SortTieBreakerByNextFastestTimePax.php
+++ Peppercorn/St1/SortTieBreakerByNextFastestTimePax.php
@@ -4,13 +4,23 @@ namespace Peppercorn\St1;
 
 class SortTieBreakerByNextFastestTimePax extends TimeSortTieBreaker
 {
+    
+    private $runCache = array();
 
     protected function getRuns(Line $line)
     {
-        $query = new Query($line->getFile());
-        return $query->where(new WhereDriverIs($line->getDriverCategory(), $line->getDriverClass(), $line->getDriverNumber()))
-                ->orderBy(SortTimePaxAscending::getSort())
-                ->executeSimple();
+        $key = $line->getDriverClassRaw() . "_" . $line->getDriverNumber();
+        $runs;
+        if (isset($this->runCache[$key])) {
+            $runs = $this->runCache[$key];
+        } else {
+            $query = new Query($line->getFile());
+            $runs = $query->where(new WhereDriverIs($line->getDriverCategory(), $line->getDriverClass(), $line->getDriverNumber()))
+                    ->orderBy(SortTimePaxAscending::getSort())
+                    ->executeSimple();
+            $this->runCache[$key] = $runs;
+        }
+        return $runs;
     }
 
     protected function getTimeForTieBreak(Line $line)
